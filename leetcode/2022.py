e7afd82a19435001860a93f26a3bc6786f487108
Zhanyl <123janyl456@gmail.com>
2025-11-09 21:48:11 +0100

    leetcode 2022

--- leetcode/2022.py
+++ leetcode/2022.py
@@ -7,7 +7,7 @@ def construct2DArray(original, m, n):
         for i in range(m):
             for j in range(n):
                 a[i][j]=original[b]
-                b+=1
+                b+=1            
     print(a)
 print(construct2DArray([2,4,3,2], 2, 2))
 
