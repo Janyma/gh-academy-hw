16dd3346ed61539e6d6e86abb2d55e25b78064e0
Zhanyl <123janyl456@gmail.com>
2025-11-09 20:53:49 +0100

    week7 hw python

---
+++ python/week7/wordcount.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python3
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+import string
+from operator import itemgetter
+
+"""Wordcount exercise
+Google's Python class
+
+The main() below is already defined and complete. It calls print_words()
+and print_top() functions which you write.
+You can use small.txt and alice.txt files to test your functions with.
+
+1. For the --count flag, implement a print_words(filename) function that counts
+how often each word appears in the text and prints:
+word1 count1
+word2 count2
+...
+
+Print the above list in order sorted by word (python will sort punctuation to
+come before letters -- that's fine). Store all the words as lowercase,
+so 'The' and 'the' count as the same word.
+
+2. For the --topcount flag, implement a print_top(filename) which is similar
+to print_words() but which prints just the top 20 most common words sorted
+so the most common word is first, then the next most common, and so on.
+
+Use str.split() (no arguments) to split on all whitespace.
+
+Workflow: don't build the whole program at once.Instead identify just a first milestone,
+e.g. "well the first step is to extract the list of words." Write the code to
+get to that milestone, and just print your data structures at that point, and
+then you can do a sys.exit(0) so the program does not run ahead into its
+not-done parts. Once the milestone code is working, you can work on code for
+the next milestone. Being able to look at the printout of your variables at one
+state can help you think about how you need to transform those variables to get
+to the next state. Python is very quick with this pattern, allowing you to make
+a little change and run the program to see how it works.
+Take advantage of that quick turnaround to build your program in little steps.
+When that's working, try for the next milestone.
+
+Optional: define a helper function to avoid code duplication inside
+print_words() and print_top().
+
+"""
+
+import sys
+
+# +++your code here+++
+# Define print_words(filename) and print_top(filename) functions.
+# You could write a helper utility function that reads a file
+# and builds and returns a word/count dict for it.
+# Then print_words() and print_top() can just call the utility function.
+
+###
+
+# This basic command line argument parsing code is provided and
+# calls the print_words() and print_top() functions which you must define.
+
+def print_words(filename):
+  with open(filename, "r") as file:
+    content = file.read() #the file will be read
+    print(content)
+    for i in string.punctuation:
+        content = content.replace(i, "") #remove unneeded chars
+
+    a=content.lower() #lowercase
+    content1= a.split() #split to words
+    content1.sort() #sort the strings
+    dictionary={}
+    for i in content1:
+      if i not in dictionary:
+        dictionary[i]=1
+      else:
+        dictionary[i]+=1
+    print(dictionary)
+
+def print_top(filename):
+  with open(filename, "r") as file:
+    content = file.read() #the file will be read
+    print(content)
+    for i in string.punctuation:
+        content = content.replace(i, "") #remove unneeded chars
+
+    a=content.lower() #lowercase
+    content1= a.split() #split to words
+    content1.sort() #sort the strings
+    dictionary={}
+    for i in content1:
+      if i not in dictionary:
+        dictionary[i]=1
+      else:
+        dictionary[i]+=1
+    sorted_data_by_value = sorted(dictionary.items(), key=itemgetter(1), reverse=True)
+    for i in range(20):
+      print("TOP", sorted_data_by_value[i])
+    #print("Print top: ", dictionary_end)
+      
+
+    print("Sorted value", sorted_data_by_value)
+print(print_top("/Users/zhanylmyrzasanzharbekova/gh-academy-hw/gh-academy-hw-1/python/week7/alice.txt"))
+                 
+
+print(print_words("/Users/zhanylmyrzasanzharbekova/gh-academy-hw/gh-academy-hw-1/python/week7/small.txt")) 
+#def print_top():
+  
+def main():
+  if len(sys.argv) != 3:
+    print('usage: ./wordcount.py {--count | --topcount} file')
+    sys.exit(1)
+
+  option = sys.argv[1]
+  filename = sys.argv[2]
+  if option == '--count':
+    print_words(filename)
+  elif option == '--topcount':
+    print_top(filename)
+  else:
+    print('unknown option: ' + option)
+    sys.exit(1)
+
+if __name__ == '__main__':
+  main()
