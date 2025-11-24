3d794aeac645a33e95f881156b4aa4d82230adee
Zhanyl <123janyl456@gmail.com>
2025-11-24 15:03:46 +0100

    hw python week 9

---
+++ python/week9/dice_rolling.py
@@ -0,0 +1,25 @@
+import random
+def dice_rolling_sum_of_two_rolls():
+    first_rolling= dice_rolling()
+    print("First rolling: ", first_rolling)
+    second_rolling= dice_rolling()
+    print("Second rolling: ", second_rolling)
+    sum= first_rolling+second_rolling
+    return sum
+
+#print(dice_rolling_sum())
+
+def dice_rolling():
+    roll= random.randint(1,6)
+    return roll
+
+def frequency_rolls():
+    rolls = int(input("How many rolls do you want? "))
+    dict_frequency = {i: 0 for i in range(2, 13)}
+    print(dict_frequency)
+    for i in range(rolls):
+        rolling_result = dice_rolling_sum_of_two_rolls()
+        dict_frequency[rolling_result]+=1
+    return dict_frequency
+
+print(frequency_rolls())
